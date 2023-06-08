cd5a38510f35f63cfa4e404d6e61771a7bce5436
David Sydykov <romulspqr0@gmail.com>
2023-06-08 11:48:32 +0600

    08.06.2023

--- resources/views/personal/main/index.blade.php
+++ resources/views/personal/main/index.blade.php
@@ -26,13 +26,17 @@
         <div class="row">
           <div class="col-lg-3 col-6">
             <!-- small box -->
+            @php
+              $liked = auth()->user()->likedPosts->count();
+              $commented = auth()->user()->commentedPosts->count();
+            @endphp
             <div class="small-box bg-info">
               <div class="inner">
-                <h3>10</h3>
+                <h3>{{ $liked }}</h3>
 
                 <p>Понравившиеся посты</p>
               </div>
-              <div class="icon">
+              <div class="icon"></div>
                 <i class="fas fa-heart"></i>
               </div>
               <a href="{{ route('personal.liked.index') }}" class="small-box-footer">Подробнее <i class="fas fa-arrow-circle-right"></i></a>
@@ -43,7 +47,7 @@
             <!-- small box -->
             <div class="small-box bg-success">
               <div class="inner">
-                <h3>10</h3>
+                <h3>{{ $commented }}</h3>
 
                 <p>Комментарии</p>
               </div>
